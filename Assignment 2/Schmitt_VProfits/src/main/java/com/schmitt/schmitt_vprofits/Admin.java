d3afe77d0d4fe71c357134c5d85ac2d4c8c3891d
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2022-09-13 10:46:30 -0500

    Complete Assignment 2

--- Assignment 2/Schmitt_VProfits/src/main/java/com/schmitt/schmitt_vprofits/Admin.java
+++ Assignment 2/Schmitt_VProfits/src/main/java/com/schmitt/schmitt_vprofits/Admin.java
@@ -2,34 +2,47 @@ package com.schmitt.schmitt_vprofits;
 
 //Import Scanner from java utilities (for user input)
 import java.util.Scanner;
-
-//Import InputmimatchException from java utilities (for validation of user input)
-import java.util.InputMismatchException;
+//Import DecimalFormatter from java text utilities (for output)
+import java.text.DecimalFormat;
+//Import java Date for more Date obj
+import java.util.Date;
 
 /**
- * This class is responsible for stating the instructions f
+ * This class is responsible for handling IO & IO Logic
  * @author Nathan Schmitt
  */
 public class Admin {
     
     String componyName;
-    double circuitBoardPrice;
-    byte circuitBoardPercentage;
+    double retailPrice;
+    double profitMargin;
     
-    
+    /**
+     * This method is responsible for introducing the program.
+     */
     protected void Intro()
     {
         System.out.println("Thsi program is used to calculate profit of "
                 + "circuit boards");
     }
     
+    /**
+     * This method is responsible for calling the Directions method repeatedly
+     * on the condition that the user would like to continue doing profit
+     * calculations.
+     * 
+     * @see Directions()
+     * 
+     * This method shall be the only method called upon from another class.
+     */
     protected void Start()
     {   
         Scanner userInput = new Scanner(System.in);
         
         System.out.println("Please enter your compony name...");
-        componyName = userInput.next();
+        componyName = userInput.next().strip();
         
+        //<editor-fold> Continue?_Logic
         boolean isDone = false;
         final String escapeChar = "N"; 
         
@@ -48,18 +61,52 @@ public class Admin {
             isDone = true;
             System.out.println("Thanks! [END OF PROGRAM]");
         }
+        //</editor-fold> Continue?_Logic
     }
     
+    
+    /**
+     * This method is responsible for gathering profit/price information and
+     * displaying results accordingly. This method does instantiate a new
+     * class.
+     * @see Calculate.java
+     */
     protected void Directions()
     {
         Scanner userInput = new Scanner(System.in);
+        DecimalFormat formatter = new DecimalFormat("$0.00");
+        Calculate calc = new Calculate();
         
+        //<editor-fold> Interactibles
         //Get the price of the circuit board from the user (NO EXCEPTION HANDLING)
-        System.out.println("Please enter circuit board price...");
-        circuitBoardPrice = userInput.nextDouble();
+        System.out.println("\nPlease enter circuit board price (Decimal Expected)...");
+        retailPrice = userInput.nextDouble();
         
         //Get the profit percentage from the user (NO EXCEPTION HANDLING)
-        System.out.println("Please enter profit percentage...");
-        circuitBoardPercentage = userInput.nextByte();  
+        System.out.println("Please enter profit percentage (Decimal < 1 expected)...");
+        profitMargin = userInput.nextDouble();
+        
+        //Calculate profits using another class (Third class was required in assignment)
+        double profits = calc.CalculateProfits(retailPrice, profitMargin);
+        //</editor-fold> Interactibles
+        
+        
+        //<editor-fold> Output_Formatting
+        //Create the output string using memory efficient concatanation
+        StringBuilder output = new StringBuilder(256);
+        
+        output.append("\n\n=======================================");
+        output.append("\n\nCompony Name:\t");
+        output.append(componyName);
+        output.append("\nDate:\t\t");
+        output.append(new Date());
+        output.append("\nRetail price:\t");
+        output.append(formatter.format(retailPrice));
+        output.append("\nProfit:\t\t");
+        output.append(formatter.format(profits));
+        //</editor-fold> Output_Fomatting
+        
+        //Print that lovely output to the screen :)
+        System.out.println(output);
     }
 }
---
+++ Assignment 2/Schmitt_VProfits/src/main/java/com/schmitt/schmitt_vprofits/Calculate.java
@@ -0,0 +1,15 @@
+package com.schmitt.schmitt_vprofits;
+
+/**
+ * @author Nathan Schmitt
+ */
+public class Calculate {
+    
+    //In the assignment it was stated that we needed at least three classes
+    //This is my third class I guess
+    
+    protected double CalculateProfits(double price, double percentage)
+    {
+        return price * percentage;
+    }
+}
